0633cbc2943b685aa064aef018bc13519fef9553
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-21 06:48:31 +0600

    write three sentences for practice backtick of ES6

--- js-problemsolving-1.js
+++ js-problemsolving-1.js
@@ -1,3 +1,10 @@
 // problem practice of recape arrow function create
 const add = (first, socond, third) => first + socond + third;
 console.log(add(10,20,30));
+
+// problem problem of ES6 backtic system
+const line  = `I am a web developer.
+I love to code.
+I love to eat biryani.
+`;
+console.log(line);
