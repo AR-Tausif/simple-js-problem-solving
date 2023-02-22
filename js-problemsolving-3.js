1dce64b4cd4dff9ac99c4245de6dfbb1980c6e03
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-22 10:01:12 +0600

    Find secondery array from data

---
+++ js-problemsolving-3.js
@@ -0,0 +1,27 @@
+/**  
+ * 1
+ * How to find secondery array from this data 
+ * get the accese secondary array with your own capacity
+ * If you want to destructuring method you can do it
+*/
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [
+                        { school_name: "ABC primary school" },
+                        { location: "Peters burg" }
+                    ]
+            },
+            {
+                secondary:
+                    [
+                        { school_name: "ABC secondary school" },
+                        { location: "St Lorence" }
+                    ]
+            },
+        ]
+    }
+}
