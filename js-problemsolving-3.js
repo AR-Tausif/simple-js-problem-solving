3c32ee3193c128789f250165c5ee46db7b0ff91b
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-22 10:05:57 +0600

    DISPLAY PRETURBUSE AND HERRY FROM THIS DATA

--- js-problemsolving-3.js
+++ js-problemsolving-3.js
@@ -60,3 +60,39 @@ let activities = {
 const resultABC = activities.activity1[0].song_list.song_1;
 const resultXYZ = activities.activity1[1].song_list.song_2;
 console.log(resultABC, resultXYZ); 
+
+
+/**  
+ * 3
+ * HOW WILL DISPLAY PRETURBUSE AND HERRY FROM THIS DATA 
+ * 1. Petersburg
+ * 2.herry
+*/
+let students = {
+    2222: {
+        name: "Jack",
+        section: "C",
+        class: "IX",
+        address: {
+            "building no": 12,
+            "street": "St. Jonson",
+            "city": "Petersburg",
+            "country": "UK"
+        }
+    },
+    3333: {
+        name: "Herry",
+        section: "D",
+        class: "X",
+        address: {
+            "building no": 85,
+            "street": "DC road",
+            "city": "Kachukhet",
+            "country": "Bangladesh"
+        }
+    }
+}
+const petersburg = students[2222].address.city;
+// console.log(petersburg);
+const herry = students[3333].name;
+// console.log(herry);
