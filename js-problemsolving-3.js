714503a50966165002d2a598a7360447c9370fb3
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-22 10:04:33 +0600

    Get accesess from two value from this data

--- js-problemsolving-3.js
+++ js-problemsolving-3.js
@@ -1,5 +1,4 @@
 /**  
- * 1
  * How to find secondery array from this data 
  * get the accese secondary array with your own capacity
  * If you want to destructuring method you can do it
@@ -25,3 +24,39 @@ let data = {
         ]
     }
 }
+const sophiaSecondarySchool = data.Sophia.study[1].secondary;
+// Destructuring Method
+// const {Sophia:{study}}=data;
+// const [{secondary}] =study;
+// console.log(sophiaSecondarySchool);
+
+
+/**
+ * 2
+ * How to get access two object property from this data list
+ * 1. abc
+ * 2.xyz
+ */
+let activities = {
+    activity1: [
+        {
+            name: "listen on spotify",
+            song_list: {
+                song_1: "abc",
+                song_2: "bcd"
+            },
+            id: 1
+        },
+        {
+            name: "listen music through bot",
+            song_list: {
+                song_1: "wxy",
+                song_2: "xyz"
+            },
+            id: 2
+        }
+    ]
+}
+const resultABC = activities.activity1[0].song_list.song_1;
+const resultXYZ = activities.activity1[1].song_list.song_2;
+console.log(resultABC, resultXYZ); 
